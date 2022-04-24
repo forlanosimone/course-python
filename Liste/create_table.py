03e84526ef8fbadb72a2d7359c8f8dbc202c4c55
Simone <simone.forlano@gmail.com>
2022-04-24 11:27:10 +0200

    def funzione

--- Liste/create_table.py
+++ Liste/create_table.py
@@ -1,6 +1,13 @@
 ## creare una tabella con dimensione specificata dall'utente
 # richiedere numero di righe e numero di colonne
 
+# funzione per stampare una matrice
+def print_2d_list(list_data):
+    for i in range(len(list_data)):                 # i si utilizza per le righe
+        for j in range(len(list_data[0])):
+            print(list_data[i][j], end=" ")         # prima si estare la sottolista e poi tutti gli elementi
+        print()                                     # sto stampando una str vuota per andare a capo
+
 n_righe = int(input("Inserisci il numero di righe:"))
 
 n_colonne = int(input("Inserisci il numero di colonne:"))
@@ -11,4 +18,5 @@ for i in range(n_righe):
     tabella.append([0] * n_colonne)
 
 print("La tabella è formata da " + str(len(tabella)) + " righe" + "e da " + str(len(tabella[0])) + " colonne")
-print(tabella)
+
+print_2d_list(tabella)
