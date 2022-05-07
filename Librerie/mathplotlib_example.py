9be4846aa0fecf7bc2c5e5751efdba4b2bfe242a
Simone <simone.forlano@gmail.com>
2022-05-07 15:00:12 +0200

    Mov

--- Librerie/mathplotlib_example.py
+++ Librerie/mathplotlib_example.py
@@ -5,6 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 fig, ax = plt.subplots() # Create a figure containing a single axes
-ax.plot([1, 2, 3, 4], [1, 4, 3, 3], "ro") # Plot some data on the axes
+ax.plot([1, 2, 3, 4], [1, 4, 3, 3]) # Plot some data on the axes
 
 plt.show()
