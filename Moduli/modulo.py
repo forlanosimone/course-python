955a5d6a7746f2529aecc35e375c1e0c738fd2ec
Simone <simone.forlano@gmail.com>
2022-05-10 19:41:27 +0200

    pylint

--- Moduli/modulo.py
+++ Moduli/modulo.py
@@ -1,10 +1,12 @@
 "Questo modulo di esempio per la somma"
 
-def somma(a,b):
-    return a+b
+def somma(a_value, b_vale):
+    "Questa funzione calcola la somma"
+    return a_value + b_vale
 
-def differenza(a,b):
-    return a-b
+def differenza(a_vale, b_value):
+    "Questa funzione calcola la differenza"
+    return a_vale - b_value
 
 print("Sto stampando qualcosa...")
 
